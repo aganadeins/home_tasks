bcd9308be15b3276061a8a33fa57358432783223
aganadeins <agatanadeina@gmail.com>
2020-11-11 14:11:55 +0300

    11.11.2020

--- mod2/Task_2_1/script.js
+++ mod2/Task_2_1/script.js
@@ -6,14 +6,14 @@ let width = 15;
 let height = 25;
 let bedArea = width * height;
 let freeSpace = allAreaInMetres % bedArea;
-console.log(freeSpace, "- оставшееся место на участке");
+console.log("Задача 1.", freeSpace, "- оставшееся место на участке");
 /* 2 задача. Найдите площадь овального кольца, полученного из овала 
 площадью 15 дм2 вырезанием овала площадью 600 см2. */
 let ovalArea = 15;
 let ovalAreaInSm = ovalArea * 100;
 let ovalForCutArea = 600;
 let ringArea = ovalAreaInSm - ovalForCutArea;
-console.log(ringArea, "(см2) - площадь овального кольца");
+console.log("Задача 2.", ringArea, "(см2) - площадь овального кольца");
 /* 3 задача. Из трех данных чисел выбрать наименьшее. */
 let num1, num2, num3;
 num1 = -90; num2 = 1; num3 = -2;
@@ -21,14 +21,14 @@ var firstCompare =
     num1 < num2 ? num1 : num2;
 var minNumber = 
     firstCompare < num3 ? firstCompare : num3;
-console.log(minNumber, "- минимальное число из трёх");
+console.log("Задача 3.", minNumber, "- минимальное число из трёх");
 /* 4 задача. Вывести в консоль ближайшее к 10 из двух чисел, записанных в переменные m и n. Например, среди чисел 8.5 и 11.45 ближайшее к десяти 11.45. */
 let m, n;
 m = 8.5;
 n = 11.45;
 var closerToTen = 
     Math.abs(n - 10) < Math.abs(m - 10) ? n : m;
-console.log(closerToTen, "- Ближайшее к 10 число");
+console.log("Задача 4.", closerToTen, "- Ближайшее к 10 число");
 /*5 задача. Трёхмерное пространство: Есть три вершины a, b и с. У каждой вершина заданы координатами x, y, z как целые числа. Нужно определить - является ли треугольник с заданными координатами прямоугольным. */
 let xa, ya, za, xb, yb, zb, xc, yc, zc;
 xa = 1; ya = 2; za = 0;
@@ -37,7 +37,7 @@ xc = 1; yc = -2; zc = 0;
 let side_ab_square = (xb - xa)**2 + (yb - ya)**2 +(zb - za)**2;
 let side_bc_square = (xb - xc)**2 + (yb - yc)**2 +(zb - zc)**2;
 let side_ac_square = (xc - xa)**2 + (yc - ya)**2 +(zc - za)**2;
-console.log(side_ab_square, side_ac_square, side_bc_square, "- это квадраты сторон!")
+console.log("Задача 5.", side_ab_square, side_ac_square, side_bc_square, "- это квадраты сторон!")
 if (side_ab_square + side_ac_square === side_bc_square) {
     console.log("Треугольник прямоугольный!");
 } else {
