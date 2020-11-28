55b7352238dc4558ad81985f4098106725b5600f
aganadeins <agatanadeina@gmail.com>
2020-11-28 07:42:23 +0300

    Хотя бы часть заданий по д.з. номер 3 и 4

--- mod2/Task_2_4/script.js
+++ mod2/Task_2_4/script.js
@@ -22,5 +22,38 @@ console.log(str2.replace(new RegExp(symbol, "ig"), symbol.repeat(2)));
 - содержит обязательно английские буквы верхнего и нижнего регистра;
 - содержит более двух цифр;
 - содержит обязательно один из неалфавитных символов (например, !, $, #, %).*/
-let psw = "u3r2jei21@!21J";
-
+let psw = prompt("Введите сложный пароль с цифрами, символами и английскими буквами");
+let len_regexp = /(.+){9}/i; // - длина от 9 символов;
+let ang_small_regexp = /[a-z]+/; // - содержит обязательно английские буквы верхнего и нижнего регистра;
+let ang_big_regexp = /[A-Z]+/; // - содержит обязательно английские буквы верхнего и нижнего регистра;
+let number_regexp = /[0-9].*[0-9]/g; // - содержит более двух цифр;
+let symbol_regexp = /[!\$#%]+/; // - содержит обязательно один из неалфавитных символов (например, !, $, #, %).
+if (len_regexp.test(psw) == 0) {
+    alert("Введите пароль не менее 9 символов");
+};
+if (ang_small_regexp.test(psw) == 0) {
+    alert("Должна быть хотя бы одна английская буква нижнего регистра")
+}
+if (ang_big_regexp.test(psw) == 0) {
+    alert("Должна быть хотя бы одна английская буква ВЕРХНЕГО регистра")
+}
+if (number_regexp.test(psw) == 0) {
+    alert("Должно быть хотя бы 2 цифры")
+}
+if (symbol_regexp.test(psw) == 0) {
+    alert("Должен быть обязательно один из неалфавитных символов (например, !, $, #, %)")
+}
+console.log(
+    len_regexp.test(psw),
+    ang_small_regexp.test(psw),
+    ang_big_regexp.test(psw),
+    number_regexp.test(psw),
+    symbol_regexp.test(psw)
+);
+console.log(
+    len_regexp.test(psw) &&
+    ang_small_regexp.test(psw) &&
+    ang_big_regexp.test(psw) &&
+    number_regexp.test(psw) &&
+    symbol_regexp.test(psw)
+);
